e0ad9e531fc16acd311c1e45d99d2a2728de5835
divas25bai-source <divas.25bai10022@vitbhopal.ac.in>
2026-03-26 11:39:49 +0530

    Update loan_project.py

--- loan_project.py
+++ loan_project.py
@@ -10,7 +10,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 
 #1) Load Data
 # Loading the dataset
-data = pd.read_csv(loan_data.csv)
+data = pd.read_csv('loan_data.csv')
 
 # Checking for nulls first thing
 print("Missing values in each column:")
